b1ee4aa7378980660d9f67d87ad4244ad2d93d0f
LEGOCHI <jcgambeta89@gmail.com>
2026-08-27 13:25:06 +0200

    Corregir URL del sitio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
 // Único valor a cambiar cuando compres el dominio.
-const SITE = process.env.SITE_URL ?? 'https://hub-enlaces.pages.dev';
+const SITE = process.env.SITE_URL ?? 'https://hub-enlaces.jcgambeta89.workers.dev';
 
 // https://astro.build/config
 export default defineConfig({

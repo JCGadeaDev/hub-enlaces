afa64458e41f2fb1a62135c44de816233659d1e3
LEGOCHI <jcgambeta89@gmail.com>
2026-09-02 12:27:41 +0200

    Direccionamiento actualizado con dominio personalizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
 // Único valor a cambiar cuando compres el dominio.
-const SITE = process.env.SITE_URL ?? 'https://hub-enlaces.jcgambeta89.workers.dev';
+const SITE = process.env.SITE_URL ?? 'https://jcgadeadev.com';
 
 // https://astro.build/config
 export default defineConfig({
